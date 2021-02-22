7ef17d56d163b2d0a93bfb22821fe0b163607195
JamieOutram <jamie.outram@btinternet.com>
2021-02-22 21:55:03 +0000

    add adapted socket code

---
+++ NetworkManager.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace UnityNetworkingLibrary
+{
+    /* Contains Highest level methods for establishing connections
+     */
+    public static class NetworkManager
+    {
+        //TODO
+    }
+}
